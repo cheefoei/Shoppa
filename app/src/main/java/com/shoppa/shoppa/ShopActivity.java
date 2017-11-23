7492df1edcd18b03113cc83d051f214206123735
Your Name <you@example.com>
2017-11-23 22:25:42 +0800

    shop activity ver2

--- app/src/main/java/com/shoppa/shoppa/ShopActivity.java
+++ app/src/main/java/com/shoppa/shoppa/ShopActivity.java
@@ -1,6 +1,11 @@
 package com.shoppa.shoppa;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +14,14 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class ShopActivity extends AppCompatActivity {
+
+    private final int PERMISSIONS_REQUEST_ACCESS_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +45,61 @@ public class ShopActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                        ShopActivity.this.checkSelfPermission(Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
 
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            PERMISSIONS_REQUEST_ACCESS_CAMERA);
+                } else {
+                    startScanBarcode();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult scanningResult =
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (scanningResult != null) {
+            if (scanningResult.getContents() != null) {
+                String shopId = scanningResult.getContents();
+                Intent intent = new Intent(this, ShopActivity.class);
+                intent.putExtra("SHOP_ID", shopId);
+                startActivity(intent);
+            }
+        } else {
+            Toast.makeText(this, "No scan data received!", Toast.LENGTH_SHORT).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScanBarcode();
+            } else {
+                Toast.makeText(this, "Shoppa need permission to scan item's barcode", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void startScanBarcode() {
+
+        IntentIntegrator integrator = new IntentIntegrator(ShopActivity.this)
+                .setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES)
+                .setPrompt("Scan product barcode")
+                .setCaptureActivity(ScannerActivity.class)
+                .setOrientationLocked(false);
+        integrator.initiateScan();
     }
 
     private void initCartLayoutComponent() {
@@ -44,13 +107,16 @@ public class ShopActivity extends AppCompatActivity {
         TextView tvCartEmpty = (TextView) findViewById(R.id.tv_cart_empty);
         tvCartEmpty.setVisibility(View.GONE);
 
+//        View alertLayout = View.inflate(this, R.layout.dialog_product_edit, null);
+//        Spinner spinner = (Spinner) alertLayout.findViewById(R.id.spinner_product_quantity);
+
         ImageButton btnProductDelete = (ImageButton) findViewById(R.id.btn_product_delete);
         btnProductDelete.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(ShopActivity.this)
+                AlertDialog.Builder builder = new AlertDialog.Builder(ShopActivity.this, R.style.DialogTheme)
                         .setTitle("Confirmation")
                         .setMessage("Remove tomato sauce?")
                         .setPositiveButton("Yes", null)
@@ -64,7 +130,12 @@ public class ShopActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
+                AlertDialog.Builder builder = new AlertDialog.Builder(ShopActivity.this, R.style.DialogTheme)
+                        .setTitle("Edit Quantity")
+                        .setView(R.layout.dialog_product_edit)
+                        .setPositiveButton("OK", null)
+                        .setNegativeButton("Cancel", null);
+                builder.show();
             }
         });
 
